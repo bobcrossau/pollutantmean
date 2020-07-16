ec8c6306cd36ac829396d751004bcc454435b519
Bob Cross <bobcross@outlook.com.au>
2020-07-16 13:08:47 +0800

    Working version

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,20 +1,17 @@
-complete1 <- function(directory, id = “flag”) {
+pollutantmean <- function(directory, pollutant, id = "flag")   {
   setwd("D:/bcross/Documents/R")
   allfiles <- list.files(directory, full.names=TRUE) #get names of files
-  fulltable <- data.frame(lapply(allfiles, read.csv)) #get data into tableframe
-  print(c(“id   nobs”, quote=FALSE)) #print heading
-        if (id == "flag") { # all datapoints required
-          id <- 1
-          for (i in 1:length(allfiles)) {
-            truecases <- (complete.cases(fulltable[id])) # true complete cases
-            nobs <- sum(truecases) #no of true cases
-            print (c(id, nobs))
-            id <- id+1
-          } else {  # only 1 datapoint required
-            truecases <- (complete.cases (fulltable[id])) # true complete cases
-            nobs <- sum(truecases) #no of true cases
-            print (c(id, nobs))
-          } 
-        }
+  fulltable <- lapply(allfiles, read.csv) #get data into tableframe
+  if (pollutant == 'sulfate')  {column <- 2} #determine if sulfate
+  if (pollutant == 'nitrate')  {column <- 3} #determine if nitrate
+  if (id == "flag") # all datapoints required
+    for (i in 1:length(allfiles)) {
+      fullcolumn <- (data.frame(fulltable[i]) [, column])
+    } 
+  if (id != "flag")  {  # only 1 datapoint required
+    fullcolumn <- (data.frame(fulltable[id]) [, column]) 
+  } 
+  m <- mean(fullcolumn, na.rm = TRUE)
+  print(m)
 }
 
