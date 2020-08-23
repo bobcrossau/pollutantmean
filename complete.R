9426061e7ef22f8c3c9e3f5a7b978d8abcc8dd46
bobcrossau <bobcross@outlook.com.au>
2020-08-23 17:44:03 +0800

    Tested version

--- complete.R
+++ complete.R
@@ -3,13 +3,14 @@ complete <- function(directory, id= 1:332) {
   allfiles <- list.files(directory, full.names=TRUE) #get names of files
   fulltable <- lapply(allfiles, read.csv) #get data into tableframe
   print(c("id    nobs"), quote=FALSE) #print heading
-  sect <- id
-  n <- min(id)
-  for (i in 1:length(id)) { # string required values together
+  n <- 1
+  for (i in seq_along(id)) { # string required values together
+    sect <- id[n]
     section <- data.frame(fulltable[sect]) #pick out sect of interest
     truecases <- complete.cases(section) # true complete cases
     nobs <- sum(truecases) #no of true cases
     print (c(sect, "  ", nobs), quote=FALSE)
-    n <- n+1
+    n <- n + 1
   } 
 }
+
