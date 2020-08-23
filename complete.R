0d47725d5aa860ab1c1c2f529156a2f70f40b421
bobcrossau <bobcross@outlook.com.au>
2020-08-23 17:41:51 +0800

    Tested version

--- complete.R
+++ complete.R
@@ -1,23 +1,15 @@
-complete <- function(directory, id="flag") {
-  setwd("D:/bcross/Documents/R")
+complete <- function(directory, id= 1:332) {
+  setwd("D:/Documents/Academic/R")
   allfiles <- list.files(directory, full.names=TRUE) #get names of files
   fulltable <- lapply(allfiles, read.csv) #get data into tableframe
   print(c("id    nobs"), quote=FALSE) #print heading
-  if (id == "flag") { # all datapoints required
-    sect <- 1
-    for (i in 1:length(allfiles)) {
-      section <- data.frame(fulltable[sect]) #pick out sect of interest
-      truecases <- complete.cases(section) # true complete cases
-      nobs <- sum(truecases) #no of true cases
-      print (c(sect, "  ", nobs), quote=FALSE)
-      sect <- sect+1
-    }
-  } 
-  if (id != "flag")  {  # only 1 datapoint required
-    sect <- id
+  sect <- id
+  n <- min(id)
+  for (i in 1:length(id)) { # string required values together
     section <- data.frame(fulltable[sect]) #pick out sect of interest
     truecases <- complete.cases(section) # true complete cases
     nobs <- sum(truecases) #no of true cases
     print (c(sect, "  ", nobs), quote=FALSE)
+    n <- n+1
   } 
 }
