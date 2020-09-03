f9980231d7acbd0a949fac5da4771912cc48b166
bobcrossau <bobcross@outlook.com.au>
2020-09-03 16:13:16 +0800

    Added output file 'cc' because of changed requirement

--- complete.R
+++ complete.R
@@ -3,14 +3,23 @@ complete <- function(directory, id= 1:332) {
   allfiles <- list.files(directory, full.names=TRUE) #get names of files
   fulltable <- lapply(allfiles, read.csv) #get data into tableframe
   print(c("id    nobs"), quote=FALSE) #print heading
-  n <- 1
+  totsect <- NULL  # this will be a vector of id's
+  totnobs <- NULL  # this will be a vector of nobs
   for (i in seq_along(id)) { # string required values together
-    sect <- id[n]
+    sect <- id[i]
     section <- data.frame(fulltable[sect]) #pick out sect of interest
     truecases <- complete.cases(section) # true complete cases
     nobs <- sum(truecases) #no of true cases
     print (c(sect, "  ", nobs), quote=FALSE)
-    n <- n + 1
-  } 
+    totsect <- append(totsect, sect) # build id vector
+    totnobs <- append(totnobs, nobs) # build nobs vector
+  }
+  cc <- data.frame(totsect, totnobs)
+  colnames(cc) <- c("id", "nobs")
+  save(cc, file = "cc.RData") #reload with load("cc.RData")
 }
+
+
+
+
 
